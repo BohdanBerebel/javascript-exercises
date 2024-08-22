520949f2d86db9d7d125e5f5e72d71dbc534c1a3
BohdanBerebel <157813971+BohdanBerebel@users.noreply.github.com>
2024-08-22 12:14:20 +0300

    Deal with the first 7 exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(arrayToFilter, ...whatDelete) {
+    let whatMustBeDeletedArray = Array.from(whatDelete);
+    let result = [];
+    mainArray: for ( i = 0; i < arrayToFilter.length; i++ ) {
+        for ( j = 0; j < whatMustBeDeletedArray.length; j++ ) {
+            if (arrayToFilter[i] === whatMustBeDeletedArray[j]) continue mainArray;
+        }
+        result.push(arrayToFilter[i]);
+    }
+    return result;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let result = 0;
 
+    if (start < 0 || end < 0) return "ERROR";
+    if (!(Number.isInteger(start) && Number.isInteger(end))) return "ERROR";
+    if (!(typeof start == 'number' && typeof end == 'number')) return "ERROR";
+    
+    if (start < end) {
+        while (start <= end) {
+            result += start;
+            start++;
+        }
+    } else {
+        while (end <= start) {
+            result += end;
+            end++;
+        }
+    }
+    return result;
 };
 
 // Do not edit below this line
